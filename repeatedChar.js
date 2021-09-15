fb0c188272ddb5c21b1581cd221229b7f375377c
Erica Basak <ericabasak@gmail.com>
2021-09-14 21:47:18 -0400

    leasted repeated practice problem

---
+++ repeatedChar.js
@@ -0,0 +1,23 @@
+// then try to solve this one, which is similar to the one we worked on:
+
+
+// given a string find the least repeated character.
+// hint: turn string into an array and try to follow the same procedure. 
+// This is good to fix the solution in your head
+
+// i.e. let str = "it is a beautiful day"
+
+// turn string into array
+// go through each element left to right
+// create an object for each element
+// return the element(s) with the lowest value (least repeated character)
+
+function leastRepeasted(str){
+    let strToArray = str.split("");
+
+    let obj = {};
+    for(let i = 0; i < str.length; i++){
+    
+    }
+}
+console.log(leastRepeasted("banana"))
