2cd7dbb7a1658f2161cfae40b9ac00878a6851ff
Erica Basak <ericabasak@gmail.com>
2021-09-16 22:32:09 -0400

    working solution for counting repetitions in array

--- countReps.js
+++ countReps.js
@@ -5,6 +5,8 @@
 // pseudocode
 // go through the array of numbers one at a time from left to right
 // compare the target number to each number in the array
+
+
 function countRepetitions(arr, target){
     let obj = {};
     for(let i = 0; i < arr.length; i++){
@@ -22,3 +24,16 @@ function countRepetitions(arr, target){
   }
   console.log(countRepetitions([3,4,5,6,1,1,3,4,1,5,1,1,6,1,1], 6));
 
+
+// solution for problem above
+function countRepetitions(arr, target){
+  let counter = 0;
+
+  for(let i = 0; i < arr.length; i++){
+    if(arr[i] === target){
+      counter += 1;
+    }
+  }
+  return counter;
+}
+console.log(countRepetitions([4,6,1,3,3,9,2,0,1,2,3,3], 3));
